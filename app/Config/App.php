7fbdcbd93a4dc00962d86016193f970505c523c4
Luciano <lnicolas15@alumnos.utalca>
2021-11-25 18:25:19 -0300

    cambio URL

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/TecnologiasWeb/public/';
+    public $baseURL = 'http://localhost/TecnologiasWeb/';
 
     /**
      * --------------------------------------------------------------------------
